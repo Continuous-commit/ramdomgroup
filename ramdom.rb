41fd094ae2c617a185eaac4a943710b960b6ca8b
Continuous-commit <fuki217@yahoo.co.jp>
2022-05-13 22:47:12 +0900

    変数名修正

--- ramdom.rb
+++ ramdom.rb
@@ -1,17 +1,17 @@
 group_array = ["A", "B", "C", "D", "E", "F"]
 
-def group_split(group_array)
-  mixgroup = group_array.shuffle
+def split_group(group_array)
+  shuffled_group = group_array.shuffle
   #3-3で分ける
   ramdom = rand(0..1)
   if ramdom == 0
-    p mixgroup.slice(0..2).sort
-    p mixgroup.slice(3..5).sort
+    p shuffled_group.slice(0..2).sort
+    p shuffled_group.slice(3..5).sort
   else
   #2-4で分ける
-    p mixgroup.slice(0..1).sort
-    p mixgroup.slice(2..5).sort
+    p shuffled_group.slice(0..1).sort
+    p shuffled_group.slice(2..5).sort
   end
 end
 
-group_split(group_array)
+split_group(group_array)
