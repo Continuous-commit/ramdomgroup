b200cfc066d937586d655b239b4c61c5dc02cd03
Continuous-commit <fuki217@yahoo.co.jp>
2022-05-13 19:29:14 +0900

    変数修正

--- ramdom.rb
+++ ramdom.rb
@@ -1,17 +1,17 @@
-array = ["A", "B", "C", "D", "E", "F"]
+group_array = ["A", "B", "C", "D", "E", "F"]
 
-def group_wake(array)
-  mazemaze = array.shuffle
+def group_split(group_array)
+  mixgroup = group_array.shuffle
   #3-3で分ける
   ramdom = rand(0..1)
   if ramdom == 0
-    p mazemaze.slice(0..2).sort
-    p mazemaze.slice(3..5).sort
+    p mixgroup.slice(0..2).sort
+    p mixgroup.slice(3..5).sort
   else
   #2-4で分ける
-    p mazemaze.slice(0..1).sort
-    p mazemaze.slice(2..5).sort
+    p mixgroup.slice(0..1).sort
+    p mixgroup.slice(2..5).sort
   end
 end
 
-group_wake(array)
+group_split(group_array)
